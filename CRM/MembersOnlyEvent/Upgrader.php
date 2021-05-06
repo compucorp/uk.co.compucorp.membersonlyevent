3e71ce821e3d37acc2f893606cb42bcc566d19f2
Ahed <ahed@compucorp.co.uk>
2021-05-06 11:08:31 +0300

    WHF-212: Add upgrader for EventGroup entity

--- CRM/MembersOnlyEvent/Upgrader.php
+++ CRM/MembersOnlyEvent/Upgrader.php
@@ -6,4 +6,15 @@ use CRM_MembersOnlyEvent_ExtensionUtil as E;
  * Collection of upgrade steps.
  */
 class CRM_MembersOnlyEvent_Upgrader extends CRM_MembersOnlyEvent_Upgrader_Base {
+
+  /**
+   * Creates membersonlyevent_event_group table
+   */
+  public function upgrade_0001() {
+    $this->ctx->log->info('Applying update 0001');
+    $this->executeSqlFile('sql/upgrade_0001.sql');
+
+    return TRUE;
+  }
+
 }
