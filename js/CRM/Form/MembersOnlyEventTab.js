cc4881278995516b88ccb30b71cde9145c5f2615
Ahed <ahed@compucorp.co.uk>
2021-05-06 11:11:56 +0300

    WHF-212: Add groups only events checkbox to MembersOnlyEventTab

--- js/CRM/Form/MembersOnlyEventTab.js
+++ js/CRM/Form/MembersOnlyEventTab.js
@@ -6,7 +6,10 @@ jQuery(document).ready(function(){
   var LINK_TYPE_URL = '1';
 
   var membersOnlyEventCheckbox= jQuery("#is_members_only_event");
+  var groupsOnlyEventCheckbox= jQuery("#is_groups_only_event");
   var membersOnlyEventFields = jQuery("#members-only-event-fields");
+  var allowedMembershipTypesField = jQuery("#allowed-membership-types-field");
+  var allowedGroupsField = jQuery("#allowed-groups-field");
 
   var purchaseButtonDisabledSection = jQuery("#purchase-button-disabled-section");
   var purchaseButtonEnabledSection = jQuery("#purchase-button-enabled-section");
@@ -22,7 +25,7 @@ jQuery(document).ready(function(){
    * the needed fields.
    */
   function setInitialFieldValues() {
-    toggleMembersOnlyEventFields(membersOnlyEventCheckbox.is(':checked'));
+    toggleTabFields();
 
     var purchaseMembershipButtonEnabled = jQuery("input[name='purchase_membership_button']:checked").val();
     togglePurchaseButtonFields(purchaseMembershipButtonEnabled);
@@ -32,11 +35,31 @@ jQuery(document).ready(function(){
   }
 
   /**
+   * Gets is_members_only_event value
+   */
+  function getIsMembersOnlyEventValue() {
+    return membersOnlyEventCheckbox.is(':checked') && !groupsOnlyEventCheckbox.is(':checked');
+  }
+
+  /**
+   * Gets is_groups_only_event value
+   */
+  function getIsGroupsOnlyEventValue() {
+    return !membersOnlyEventCheckbox.is(':checked') && groupsOnlyEventCheckbox.is(':checked');
+  }
+
+  /**
    * Sets the fields event listeners
    */
   function setFieldListeners() {
     membersOnlyEventCheckbox.click(function(){
-      toggleMembersOnlyEventFields(jQuery(this).is(':checked'));
+      groupsOnlyEventCheckbox.prop('checked', false);
+      toggleTabFields();
+    });
+
+    groupsOnlyEventCheckbox.click(function(){
+      membersOnlyEventCheckbox.prop('checked', false);
+      toggleTabFields();
     });
 
     jQuery("input[name='purchase_membership_button']").click(function(){
@@ -49,14 +72,25 @@ jQuery(document).ready(function(){
   }
 
   /**
-   * Shows/Hides the members-only events fields
+   * Shows/Hides the tab fields
    * based on 'Is members-only event ?' checkbox
    * value.
-   *
-   * @param isMembersOnlyEvent
    */
-  function toggleMembersOnlyEventFields(isMembersOnlyEvent) {
-    if (isMembersOnlyEvent){
+  function toggleTabFields() {
+    let isMembersOnlyEventValue = getIsMembersOnlyEventValue();
+    let isGroupsOnlyEventValue = getIsGroupsOnlyEventValue();
+
+    if (isMembersOnlyEventValue || isGroupsOnlyEventValue){
+      if (isMembersOnlyEventValue) {
+        allowedMembershipTypesField.show();
+        allowedGroupsField.hide();
+      }
+
+      if (isGroupsOnlyEventValue) {
+        allowedMembershipTypesField.hide();
+        allowedGroupsField.show();
+      }
+
       membersOnlyEventFields.show();
     } else {
       membersOnlyEventFields.hide();
