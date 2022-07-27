330207e3ed3bb7792838d10f3a775df7016c7196
Ahed <ahed@compucorp.co.uk>
2022-07-27 10:04:45 +0300

    WHF-308: Add an upgrader to add new membership columns

--- CRM/MembersOnlyEvent/Upgrader.php
+++ CRM/MembersOnlyEvent/Upgrader.php
@@ -28,6 +28,8 @@ class CRM_MembersOnlyEvent_Upgrader extends CRM_MembersOnlyEvent_Upgrader_Base {
    * - Add is_showing_login_block column.
    * - Add block_type column.
    * - Add login_block_message column.
+   * - Add is_showing_purchase_membership_block column.
+   * - Add purchase_membership_body_text column.
    */
   public function upgrade_0002() {
     $this->ctx->log->info('Applying update 0002');
--- sql/upgrade_0002.sql
+++ sql/upgrade_0002.sql
@@ -37,3 +37,14 @@ ALTER TABLE `membersonlyevent` ADD `is_showing_custom_access_denied_message` tin
 ALTER TABLE `membersonlyevent` ADD `is_showing_login_block` tinyint DEFAULT 0 COMMENT 'This allows you to choose a Login block to display and add a custom message to be displayed on the block for anonymous users' AFTER `notice_for_access_denied`;
 ALTER TABLE `membersonlyevent` ADD `block_type` int unsigned DEFAULT 1 COMMENT 'Login block type' AFTER `is_showing_login_block`;
 ALTER TABLE `membersonlyevent` ADD `login_block_message` text DEFAULT NULL COMMENT 'Login block message to show to the user when access to members-only event denied.' AFTER `block_type`;
+
+-- /*******************************************************
+-- *
+-- * membersonlyevent
+-- *
+-- * Add is_showing_purchase_membership_block column
+-- * Add purchase_membership_body_text column
+-- *
+-- *******************************************************/
+ALTER TABLE `membersonlyevent` ADD `is_showing_purchase_membership_block` tinyint DEFAULT 0 COMMENT 'This allows you to add a label, custom message and  link to be displayed on the event.' AFTER `login_block_message`;
+ALTER TABLE `membersonlyevent` ADD `purchase_membership_body_text` text DEFAULT NULL COMMENT 'Custom message to show to the user when access to members-only event denied.' AFTER `purchase_membership_button_label`;
