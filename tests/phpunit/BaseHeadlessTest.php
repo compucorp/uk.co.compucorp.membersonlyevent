4f54107c3000844211660454c779a5af05aa1e94
Muhammad Shahrukh <>
2024-07-29 12:35:25 +0500

    COMCL-635: Upgrade civix

--- tests/phpunit/BaseHeadlessTest.php
+++ tests/phpunit/BaseHeadlessTest.php
@@ -3,11 +3,12 @@
 use Civi\Test;
 use Civi\Test\HeadlessInterface;
 use Civi\Test\TransactionalInterface;
+use PHPUnit\Framework\TestCase;
 
 /**
  * An abstract BaseHeadlessTest class.
  */
-abstract class BaseHeadlessTest extends PHPUnit_Framework_TestCase implements HeadlessInterface, TransactionalInterface, Test\HookInterface {
+abstract class BaseHeadlessTest extends TestCase implements HeadlessInterface, TransactionalInterface, Test\HookInterface {
 
   /**
    * Sets up Headless, use stock schema,, install extensions.
